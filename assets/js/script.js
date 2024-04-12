16aec007a09a765f146b7bcada58764bc279a8d9
Michael Young <michaelyoung@michaels-air.mynetworksettings.com>
2024-04-11 20:24:28 -0400

    created test site for api, it worked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,8 @@ function fetchDogs() {
     const energy = document.getElementById('energy').value;
     const shedding = document.getElementById('shedding').value;
     const weight = document.getElementById('weight').value;
-  
     const url = `https://api.api-ninjas.com/v1/dogs?energy=${energy}&shedding=${shedding}&min_weight=${weight}`;
-
-    var name = 'golden retriever'
+    // var name = 'golden retriever'
     $.ajax({
         method: 'GET',
         url: url,
@@ -14,7 +12,6 @@ function fetchDogs() {
         contentType: 'application/json',
         success: function(result) {
             console.log(result);
-
             if (result.length === 0){
                 console.log("Nah B guess again");
             }
@@ -23,55 +20,4 @@ function fetchDogs() {
             console.error('Error: ', jqXHR.responseText);
         }
     });
-
-
-
-
-
 }
-
-//     fetch(url, {
-//         // mode: 'no-cors'
-//     })
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         if (data.length < 5) {
-//           alert("There are less than 5 results based on the given parameters.");
-//         } else {
-//           displayDogs(data);
-//         }
-//       })
-//       .catch(error => {
-//         console.error('There was a problem with the fetch operation:', error);
-//       });
-//   }
-  
-//   function displayDogs(dogs) {
-//     const dogsContainer = document.getElementById('dogs');
-//     dogsContainer.innerHTML = '';
-  
-//     dogs.forEach(dog => {
-//       const dogElement = document.createElement('div');
-//       dogElement.textContent = `${dog.name}: Energy - ${dog.energy}, Shedding - ${dog.shedding}, Weight - ${dog.weight} lbs`;
-//       dogsContainer.appendChild(dogElement);
-//     });
-//   }
-  
-// var name = 'golden retriever'
-// $.ajax({
-//     method: 'GET',
-//     url: url,
-//     headers: { 'X-Api-Key': '+SZxtNXiFqfl0H6PZw8wVQ==v7XBFa53qMupptej'},
-//     contentType: 'application/json',
-//     success: function(result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
