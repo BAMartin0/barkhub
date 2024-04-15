c9919812c633374ea735a8fd33ebae77120db32e
B. Martin <imbrittanyalexis@gmail.com>
2024-04-15 19:22:50 -0400

    added random result feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
 function fetchDogs() {
-    const apiKey = '+SZxtNXiFqfl0H6PZw8wVQ==v7XBFa53qMupptej'; // Replace with your API key
-    const energy = document.getElementById('energy').value;
-    const shedding = document.getElementById('shedding').value;
-    const weight = document.getElementById('weight').value;
-    const barking = document.getElementById('barking').value;
-    const protectiveness = document.getElementById('protectiveness').value;
+  const apiKey = "+SZxtNXiFqfl0H6PZw8wVQ==v7XBFa53qMupptej"; // Replace with your API key
+  const energy = document.getElementById("energy").value;
+  const shedding = document.getElementById("shedding").value;
+  const weight = document.getElementById("weight").value;
+  const barking = document.getElementById("barking").value;
+  const protectiveness = document.getElementById("protectiveness").value;
 
-    const url = `https://api.api-ninjas.com/v1/dogs?energy=${energy}&shedding=${shedding}&max_weight=${weight}&barking=${barking}&protectiveness=${protectiveness}`;
-// changed min_weight to max_weight
-    var xhr = new XMLHttpRequest();
-    var url2 = "your_url_here";
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader("X-Api-Key", "+SZxtNXiFqfl0H6PZw8wVQ==v7XBFa53qMupptej");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
+  const url = `https://api.api-ninjas.com/v1/dogs?energy=${energy}&shedding=${shedding}&max_weight=${weight}&barking=${barking}&protectiveness=${protectiveness}`;
+  // changed min_weight to max_weight
+  var xhr = new XMLHttpRequest();
+  var url2 = "your_url_here";
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("X-Api-Key", "+SZxtNXiFqfl0H6PZw8wVQ==v7XBFa53qMupptej");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-            var result = JSON.parse(xhr.responseText);
-            console.log(result);
-            if (result.length === 0) {
-                console.log("Nah B guess again");
-            } else {
-                    const matchedArray = JSON.parse(localStorage.getItem("matchedArray")) || [];
-                    matchedArray.push(result[0]);
-                    console.log("Updated matchedArray:", matchedArray);
-                    localStorage.setItem("matchedArray", JSON.stringify(matchedArray));
-                }
+      if (xhr.status === 200) {
+        var result = JSON.parse(xhr.responseText);
+        console.log(result);
+        if (result.length === 0) {
+          console.log("Nah B guess again");
         } else {
-            console.error('Error: ', xhr.responseText);
+          const randomIndex = Math.floor(Math.random() * result.length);
+          const randomResult = result[randomIndex];
+          console.log("Random result:", randomResult);
+          const matchedArray =
+            JSON.parse(localStorage.getItem("matchedArray")) || [];
+          matchedArray.push(result.random);
+          // console.log("Updated matchedArray:", matchedArray);
+          localStorage.setItem("matchedArray", JSON.stringify(matchedArray));
         }
+      } else {
+        console.error("Error: ", xhr.responseText);
+      }
     }
-};
-xhr.send();
+  };
+  xhr.send();
 }
-
